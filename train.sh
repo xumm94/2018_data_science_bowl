1fbd946b74fe6a0e14bb43e23a675141110a76b1
Jeff-LiangF <liangfjeff@gmail.com>
2018-03-06 16:47:45 +0800

    The bash scripts to train the model with head

--- train.sh
+++ train.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
 
-export CUDA_VISIBLE_DEVICES=2
+export CUDA_VISIBLE_DEVICES=6,7
 
-python ./train_nuclei.py \
-    train \
-    --model ./pretrained_model/mask_rcnn_coco.h5 \
-    --ckpt ./expr/ \
-    --dataset ./data/ \
-    --epochs  40\
-    --finetune all \
+for ((i=1; i<=10; i++))
+do
+    train_dataset=10-fold-train-$i.txt
+    val_dataset=10-fold-val-$i.txt
+
+    python ./train_nuclei.py \
+        train \
+        --model ./models/mask_rcnn_coco.h5 \
+        --ckpt /data/lf/Nuclei/logs-head \
+        --datapath ./data/ \
+        --epochs  50\
+        --finetune heads \
+        --lr_start 0.01 \
+        --train_dataset $train_dataset \
+        --val_dataset $val_dataset
+
+done
